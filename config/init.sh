af72b6763b24203200273d2abfac30fe33784a73
tortxoFFoxtrot <155745095+tortxoFFoxtrot@users.noreply.github.com>
2024-07-29 04:47:53 +1000

    tweaks to follow the way I am doing it with Bitcoin

--- config/init.sh
+++ config/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sleep infinity
+exec sudo -H -u www-data monerod --non-interactive=1 --config-file=/config/monerod.conf
---
+++ config/tor/monero-service/README.md
@@ -0,0 +1,1 @@
+point tor hidden service to here, place our vanity onion etc
