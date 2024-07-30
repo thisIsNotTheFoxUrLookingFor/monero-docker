354352b00262d67f4996bae6b9b3d88d86dae32d
tortxoFFoxtrot <155745095+tortxoFFoxtrot@users.noreply.github.com>
2024-07-31 02:55:51 +1000

    gen new ssl certs at startup before lowering permission

--- config/init.sh
+++ config/init.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-exec monerod --non-interactive --config-file=/config/monerod.conf
+monero-gen-ssl-cert --certificate-filename=/monero/rpc_ssl.crt --private-key-filename=/monero/rpc_ssl.key
+chown root:www-data /monero/rpc_ssl.key
+chown root:www-data /monero/rpc_ssl.crt
+exec sudo -H -u www-data monerod --non-interactive --config-file=/config/monerod.conf
