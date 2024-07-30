af3928613ff0fe09377806498fe7397fbe579485
tortxoFFoxtrot <155745095+tortxoFFoxtrot@users.noreply.github.com>
2024-07-31 08:24:34 +1000

    build actual release bins and disable stack trace on exception

--- config/init.sh
+++ config/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 monero-gen-ssl-cert --certificate-filename=/monero/rpc_ssl.crt --private-key-filename=/monero/rpc_ssl.key
-chown root:www-data /monero/rpc_ssl.key
-chown root:www-data /monero/rpc_ssl.crt
+chown www-data:www-data /monero/rpc_ssl.key
+chown www-data:www-data /monero/rpc_ssl.crt
 exec sudo -H -u www-data monerod --non-interactive --config-file=/config/monerod.conf
